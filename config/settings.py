e822a130e6286c6eefb8e8ebaee0202f5bc9f0c6
Yaroslav <bartsismon333@gmail.com>
2025-09-18 21:17:27 +0200

    several commit

--- config/settings.py
+++ config/settings.py
@@ -81,11 +81,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='postgres://user:pass@host:5432/dbname')
 }
 
 # Password validation
