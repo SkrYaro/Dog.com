7694af96cd22dd5d4877067a0605872b72db7255
Yaroslav <bartsismon333@gmail.com>
2025-09-19 15:53:20 +0200

    some changes

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY","ccx^j8$s*bb7xv@tf6@1=!@6y!b1r$k_h0v##lcd3)k0(zjy1%")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  os.environ.get('DEBUG',True)
+DEBUG =  os.environ.get('DEBUG',False)
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1').split(',')
 
